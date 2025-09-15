97267d7b9ef68ace8a74b48b9559db4e822f1b59
MDAdrian <adi.maniu33@gmail.com>
2025-09-15 16:41:22 +0300

    HealthZ

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ import (
 	"net/http"
 )
 
+
 func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
+	// Serve files under /app/
+	appHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", appHandler)
+
+	// Serve files from the "assets" directory under /assets/
+	mux.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+	})
+
+	// Health (readiness/liveness) endpoint
+	mux.HandleFunc("/healthz", readinessHandler)
+
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -17,4 +31,10 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
