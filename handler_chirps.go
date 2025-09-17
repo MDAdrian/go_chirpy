9dfd5f2647d5d6528506745f6ebb8d8c9ef44e5d
MDAdrian <adi.maniu33@gmail.com>
2025-09-17 16:49:33 +0300

    Get Chirps

--- handler_chirps.go
+++ handler_chirps.go
@@ -11,17 +11,16 @@ import (
 	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+type Chirp struct {
+	Id uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`	
+	UpdatedAt time.Time `json:"updated_at"`
+	Body string `json:"body"`
+	UserId uuid.UUID `json:"user_id"`
+}
 
+func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
 	type createChirpRequest struct {
-		Body string `json:"body"`
-		UserId uuid.UUID `json:"user_id"`
-	}
-	type createChirpResponse struct {
-		Id uuid.UUID `json:"id"`
-		CreatedAt time.Time `json:"created_at"`	
-		UpdatedAt time.Time `json:"updated_at"`
 		Body string `json:"body"`
 		UserId uuid.UUID `json:"user_id"`
 	}
@@ -45,11 +44,33 @@ func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request)
 	}
 
 	fmt.Printf("Created chirp for user: %s, message: '%s'\n", chirpEntity.UserID, chirpEntity.Body)
-	respondWithJSON(w, http.StatusCreated, createChirpResponse {
+	respondWithJSON(w, http.StatusCreated, Chirp {
 		Id: chirpEntity.ID,
 		CreatedAt: chirpEntity.CreatedAt,
 		UpdatedAt: chirpEntity.UpdatedAt,
 		Body: chirpEntity.Body,
 		UserId: chirpEntity.UserID,
 	})
+}
+
+func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
+	chirps, err := cfg.db.GetChirps(context.Background())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error when getting chirps: %w", err)
+		return
+	}
+
+	chirps_converted := []Chirp{} // initialize an empty slice
+
+	for _, chirp := range chirps {
+		chirps_converted = append(chirps_converted, Chirp{
+			Id:        chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
+			UserId:    chirp.UserID,
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, chirps_converted)
 }
